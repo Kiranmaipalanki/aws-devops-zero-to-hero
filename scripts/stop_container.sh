c66353d6082c70bdd82f96aea5f4254023922bed
Kiranmai <kiranmaipalanki4@gmail.com>
2025-11-10 12:33:53 +0530

    xyz

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# Stop the running container(s), if any
-containerid=$(docker ps -q)
+# Find container using port 5000
+containerid=$(docker ps --filter "publish=5000" -q)
 
 if [ -n "$containerid" ]; then
   docker rm -f $containerid
 else
-  echo "No running containers to stop."
+  echo "No container running on port 5000."
 fi
