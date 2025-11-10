be291789dbb63aab63f88cb3fe966f925358b409
Kiranmai <kiranmaipalanki4@gmail.com>
2025-11-10 12:09:55 +0530

    commit my changes

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hi, this is Kiranmai from India! How are you?'
+    return 'Hi, this is Kiranmai from India!'
 
 if __name__ == '__main__':
     app.run()
